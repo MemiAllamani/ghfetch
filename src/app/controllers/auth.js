cbd10fe7309c8778d87115d58b13898facda4a03
Memi Allamani <memi.allamani@gmail.com>
2018-08-18 14:42:22 +0200

    Implement login and logout handler

--- src/app/controllers/auth.js
+++ src/app/controllers/auth.js
@@ -1,14 +1,53 @@
 'use strict';
 
+const JWT = require('jsonwebtoken');
+
+const githubService = require('../services/github');
+const sessionService = require('../services/session');
+const userProfileService = require('../services/profile');
+const jwtSecret = require('../configs').authentication.key;
+
+
 module.exports = {
     login: {
+        auth: false,
         handler: (request, h) => {
-            return 'user logged in';
+            const {username, password} = request.payload
+            const ghClient = githubService.getClient(username, password);
+            let ghProfile = null;
+            try {
+                ghProfile = await githubService.getProfile(ghClient);
+            } catch (err) {
+                console.log('Authentication failed: ', err);
+            }
+
+            if (!ghProfile) {
+                Boom.unauthorized('Invalid username or password');
+            }
+
+            const sessionId = await sessionService.create({
+                username, password
+            });
+            await userProfileService.create({
+                name: ghProfile.name,
+                company: ghProfile.company,
+                location: ghProfile.location,
+                avatarUrl: ghProfile.avatarUrl,
+                followers: ghProfile.followers,
+                following: ghProfile.following,
+                session_id: sessionId
+            });
+
+            return {
+                token: JWT.sign({id: sessionId}, jwtSecret)
+            };
         }
     },
     logout: {
+        auth: true,
         handler: (request, h) => {
-            return 'user logged out';
+            const sessionId = request.auth.credentials.id;
+            await sessionService.delete(sessionId);
         }    
     }
 }
