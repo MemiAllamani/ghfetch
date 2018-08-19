27197b442c53a247ce95ac141a135cd31046ff82
Memi Allamani <memi.allamani@gmail.com>
2018-08-19 14:34:09 +0200

    Implement auth service

--- src/public/src/app/app.module.ts
+++ src/public/src/app/app.module.ts
@@ -1,21 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { Routes, RouterModule } from '@angular/router';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login.component';
 import { ProfileComponent } from './components/profile.component';
+import { ErrorInterceptor, JwtInterceptor } from './services/auth.service';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes : Routes = [{
   path: 'login',
   component: LoginComponent
 }, {
   path: 'profile',
-  component: ProfileComponent
+  component: ProfileComponent,
+  canActivate: [AuthGuard]
 }, {
-  path: '',
+  path: '**',
   redirectTo: 'profile',
   pathMatch: 'full'
 }];
@@ -24,11 +33,20 @@ const routes : Routes = [{
   imports: [
     CommonModule,
     BrowserModule,
+    NoopAnimationsModule,
     RouterModule.forRoot(routes, {useHash: true}),
+    HttpClientModule,
     MatToolbarModule,
-    MatButtonModule
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule,
+    MatProgressSpinnerModule
   ],
   declarations: [AppComponent, LoginComponent, ProfileComponent],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/public/src/app/services/auth.service.ts
+++ src/public/src/app/services/auth.service.ts
@@ -1,9 +1,82 @@
 import { Injectable } from '@angular/core';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+
+
+/** Model */
+
+export class User {
+  token: string
+}
+
+/** Service */
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  login(user : string, pass: string) {
+    return this.http.post<any>('/auth/login', { user, pass })
+    .pipe(map(user => {
+        if (user && user.token) {
+          localStorage.setItem('currentUser', JSON.stringify(user));
+        }
+        return user;
+    }));
+  }
+
+  getLoggedInUser() : User {
+    return JSON.parse(localStorage.getItem('currentUser'));
+  }
+
+  logout() {
+    localStorage.removeItem('currentUser');
+  }
+}
+
+
+/**
+ * Authentication error interceptor
+ */
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+  constructor(private authService: AuthService) {}
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+      return next.handle(request).pipe(catchError(err => {
+          if (err.status === 401) {
+              this.authService.logout();
+              location.reload(true);
+          }
+          
+          const error = err.error.message || err.statusText;
+          return throwError(error);
+      }))
+  }
+}
+
+/**
+ * Authentication jwt token interceptor
+ */
+@Injectable()
+export class JwtInterceptor implements HttpInterceptor {
+  
+  constructor(private authService: AuthService) {}
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    let currentUser = this.authService.getLoggedInUser();
+    if (currentUser && currentUser.token) {
+      request = request.clone({
+          setHeaders: { 
+              Authorization: `${currentUser.token}`
+          }
+      });
+    }
+    
+    return next.handle(request);
+  }
 }
